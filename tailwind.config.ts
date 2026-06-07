32963df52877052380f2f577b0ef38027ac63ead
Catsu <dcristianyuri@gmail.com>
2026-06-07 17:24:04 +0800

    commited color branding in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        navy: '#0D1428',
+        yellow: '#F0C419',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
